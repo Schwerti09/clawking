7a89d309b99a873f16037dbf8b70268e2bf7b0cb
Schwerti09 <rps-vertrieb@t-online.de>
2026-03-05 23:56:47 +0100

    Delete lib/analytics.ts

--- lib/analytics.ts
+++
@@ -1,4 +0,0 @@
-export const trackEvent = () => {};
-export const incrementCheckCount = () => 0;
-export const getCheckCount = () => 0;
-export default { trackEvent, incrementCheckCount, getCheckCount };
