47010d1e11f52ea6c3eb3872f95b3661b6c72024
Schwerti09 <rps-vertrieb@t-online.de>
2026-03-05 12:39:00 +0100

    Simplify analytics module by removing functions
    Removed unused functions and streamlined exports.

--- lib/analytics.ts
+++ lib/analytics.ts
@@ -1,4 +1,2 @@
-export const trackEvent = (..._args: unknown[]) => {};
-export const incrementCheckCount = (): number => 0;
-export const getCheckCount = (): number => 0;
-export default { trackEvent, incrementCheckCount, getCheckCount };
+export const trackEvent = () => {};
+export default { trackEvent };
