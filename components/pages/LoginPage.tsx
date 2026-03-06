96d12aca44f7d109cce317e6fc206fdf8ada4d9c
Schwerti09 <rps-vertrieb@t-online.de>
2026-03-06 23:52:48 +0100

    fix: add error prop to LoginPage component

--- components/pages/LoginPage.tsx
+++ components/pages/LoginPage.tsx
@@ -1,7 +1,16 @@
-import React from 'react'; 
- 
-const LoginPage: React.FC = () => { 
-  return <div>Login Page Component</div>; 
-}; 
- 
-export default LoginPage; 
+import React from 'react';
+
+type LoginPageProps = {
+  error?: string;
+};
+
+const LoginPage: React.FC<LoginPageProps> = ({ error }) => {
+  return (
+    <div>
+      <h1>Login Page</h1>
+      {error && <p style={{ color: 'red' }}>Error: {error}</p>}
+    </div>
+  );
+};
+
+export default LoginPage;
