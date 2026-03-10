e39812fa7817a83a7c60228d20aa9688bdbf5402
Schwerti09 <rps-vertrieb@t-online.de>
2026-03-10 20:00:52 +0100

    Fix next.config.js syntax

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
-﻿(
-echo /** @type {import('next').NextConfig} */
-echo const nextConfig = {
-echo   eslint: { ignoreDuringBuilds: true },
-echo   typescript: { ignoreBuildErrors: true },
-echo };
-echo module.exports = nextConfig;
-) > next.config.js
+﻿/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+};
+
+module.exports = nextConfig;
