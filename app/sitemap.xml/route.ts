ca79728051eff8535577360077943c70fa6baa82
Schwerti09 <rps-vertrieb@t-online.de>
2026-03-11 22:49:18 +0100

    FIX: sitemap.xml/route.ts korrekt angelegt + sitemapindex wiederhergestellt

---
+++ app/sitemap.xml/route.ts
@@ -0,0 +1,33 @@
+// app/sitemap.xml/route.ts
+import { NextResponse } from 'next/server';
+
+const BASE_URL = 'https://clawguru.org';
+
+export async function GET() {
+  const today = new Date().toISOString().split('T')[0];
+
+  const subSitemaps = [
+    { loc: `${BASE_URL}/sitemap/runbooks.xml`, lastmod: today },
+    { loc: `${BASE_URL}/sitemap/providers.xml`, lastmod: today },
+    { loc: `${BASE_URL}/sitemap/tags.xml`, lastmod: today },
+    // ... alle deine echten Sub-Sitemaps hier eintragen!
+  ];
+
+  const xml = `<?xml version="1.0" encoding="UTF-8"?>
+<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${subSitemaps.map(s => `
+  <sitemap>
+    <loc>${s.loc}</loc>
+    <lastmod>${s.lastmod}</lastmod>
+  </sitemap>
+`).join('')}
+</sitemapindex>`;
+
+  return new NextResponse(xml, {
+    status: 200,
+    headers: {
+      'Content-Type': 'application/xml',
+      'Cache-Control': 'public, max-age=3600',
+    },
+  });
+}
